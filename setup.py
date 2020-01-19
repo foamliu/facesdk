669754c3667a7238f18c1858d2f61e8842400ec7
Yang Liu <liuyang12@focusmedia.cn>
2020-01-19 10:05:57 +0800

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="facesdk",
-    version="0.0.7",
+    version="0.0.8",
     author="Yang Liu",
     author_email="foamliu@yeah.net",
     url="https://github.com/foamliu/FaceSDK",
